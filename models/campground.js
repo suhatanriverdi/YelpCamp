0012863862f2f0bf6a3f7757b2fc22ccc2c20374
shtanriverdi <shtanriverdi@yahoo.com>
2023-10-25 08:40:59 +0300

    Add remove reviews associated with a campground

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Review = require('./review');
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
@@ -13,6 +14,19 @@ const CampgroundSchema = new Schema({
             ref: 'Review'
         }
     ]
+});
+
+// This middleware will run once we remove a campground, after we remove
+CampgroundSchema.post('findOneAndDelete', async function (deletedCampground) {
+    // We have access the removed thing here by passing in, "deletedCampground"
+    console.log("Removed Campground: ", deletedCampground);
+    if (deletedCampground) {
+        await Review.deleteMany({
+            _id: {
+                $in: deletedCampground.reviews
+            }
+        });
+    }
 });
 
 module.exports = mongoose.model('Campground', CampgroundSchema);
