8eb8a664aff1ea2a1552cacf06a1aba128c6a30f
shtanriverdi <shtanriverdi@yahoo.com>
2023-10-30 17:55:26 +0300

    modified:   routes/users.js 	modified:   views/users/login.ejs

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
+const passport = require('passport');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
@@ -20,12 +21,22 @@ router.post('/register', catchAsync(async (req, res) => {
         res.redirect('/campgrounds');
     } catch (err) {
         req.flash('error', err.message);
+
+        // BUNU DENE EKSTRA APP YAZ DENE
         res.redirect('/register');
     }
 }))
 
 router.get('/login', (req, res) => {
     res.render('users/login')
+})
+
+/* passport library gives us a middleware we can use for auth */
+router.post('/login', passport.authenticate('local',
+    { failureFlash: true, failureRedirect: '/login' }),
+    (req, res) => {
+        req.flash('success', 'Welcome back!');
+        res.redirect('/campgrounds');
 })
 
 module.exports = router;
