69a8e62fdd51e735b65a5fad595d46f4ae02b7fd
shtanriverdi <shtanriverdi@yahoo.com>
2023-10-31 19:06:09 +0300

    File Upload Intro Multer

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -10,9 +10,17 @@ const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 
+// This will help parsing the "enctype="multipart/form-data"" for image uploading part
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    .post(upload.array('image'), (req, res) => {
+        console.log(req.body, req.files);
+        res.send("IT WORKED")
+    })
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
