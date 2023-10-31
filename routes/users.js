eb25fd21d82ac08771e661216060c07100333ca7
shtanriverdi <shtanriverdi@yahoo.com>
2023-10-31 15:41:57 +0300

    router.route('/register')

--- routes/users.js
+++ routes/users.js
@@ -8,21 +8,21 @@ const { storeReturnTo } = require('../middleware');
 // MVC Controllers
 const users = require('../controllers/users');
 
-router.get('/register', users.renderRegister)
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.registerUser))
 
-router.post('/register', catchAsync(users.registerUser))
-
-router.get('/login', users.renderLogin)
-
-/* passport library gives us a middleware we can use for auth */
-router.post('/login',
-    // use the storeReturnTo middleware to save the returnTo value from session to res.locals   
-    storeReturnTo,
-    // passport.authenticate logs the user in and clears req.session
-    passport.authenticate('local',
-        { failureFlash: true, failureRedirect: '/login' }),
-    // Now we can use res.locals.returnTo to redirect the user after login
-    users.login)
+router.route('/login')
+    .get(users.renderLogin)
+    /* passport library gives us a middleware we can use for auth */
+    .post(
+        // use the storeReturnTo middleware to save the returnTo value from session to res.locals   
+        storeReturnTo,
+        // passport.authenticate logs the user in and clears req.session
+        passport.authenticate('local',
+            { failureFlash: true, failureRedirect: '/login' }),
+        // Now we can use res.locals.returnTo to redirect the user after login
+        users.login)
 
 router.get('/logout', users.logout);
 
